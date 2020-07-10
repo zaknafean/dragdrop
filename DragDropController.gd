ef5ae466f5a2c044503ef133fea4e7b28d61069b
zaknafean <zaknafean@gmail.com>
2020-07-10 08:57:37 -0400

    Update DragDropController.gd
    Added ability to drag and drop Control type nodes
    Added ability to clamp drag and drop to visible screen

--- DragDropController.gd
+++ DragDropController.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 var current = null
 var drag_offset = Vector2()
@@ -6,19 +6,37 @@ var drag_offset = Vector2()
 var candidates = []
 
 export var drag_group = "draggable"
+export (bool) var clamp_to_screen = false
+
+onready var screen_size = get_viewport_rect().size
 
 
 func _ready():
 	var draggables = get_tree().get_nodes_in_group(drag_group)
 	for dragable in draggables:
-		if dragable is CollisionObject2D:
+		if dragable is CollisionObject2D or dragable is Control:
 			dragable.connect("mouse_entered",self,"mouse_entered",[dragable])
 			dragable.connect("mouse_exited",self,"mouse_exited",[dragable])
-			dragable.connect("input_event",self,"input_event",[dragable])
+			if dragable is CollisionObject2D:
+				dragable.connect("input_event",self,"input_event",[dragable])
+			elif dragable is Control:
+				dragable.connect("gui_input",self,"input_event_control")
 
-func _process(delta):
-	if current is Node2D:
-		current.global_position = current.get_global_mouse_position() - drag_offset
+func _process(_delta):
+	if current:
+		var newPosition = current.get_global_mouse_position() - drag_offset
+		
+		if clamp_to_screen:
+			if newPosition.x > screen_size.x - 64:
+				newPosition.x = screen_size.x - 64
+			if newPosition.x < 0:
+				newPosition.x = 0
+			if newPosition.y > screen_size.y - 64:
+				newPosition.y = screen_size.y - 64
+			if newPosition.y < 0:
+				newPosition.y = 0
+		
+		current.set_global_position(newPosition)
 
 func mouse_entered(which):
 	candidates.append(which)
@@ -28,15 +46,17 @@ func mouse_exited(which):
 	candidates.erase(which)
 	pass
 
-func input_event(viewport: Node, event: InputEvent, shape_idx: int,which:Node2D):
+func input_event(_viewport: Node, event: InputEvent, _shape_idx: int, _which: Node2D):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 		if event.is_pressed():
 			candidates.sort_custom(self,"depth_sort")
-			var last = candidates.back()
+			var last = null
+			if candidates.size() > 0:
+				last = candidates.back()
 			if last:
 				last.raise()
 				current = last
-				drag_offset = current.get_global_mouse_position() - current.global_position
+				drag_offset = current.get_global_mouse_position() - current.get_global_position()
 				if current.has_method("on_drag_start"):
 					current.on_drag_start()
 		else:
@@ -48,5 +68,9 @@ func input_event(viewport: Node, event: InputEvent, shape_idx: int,which:Node2D)
 				if can_drop:
 					current = null
 
-func depth_sort(a,b):
-	return b.get_index()<a.get_index()
+func input_event_control(event: InputEvent):
+	input_event(null, event, 0, null)
+	
+
+func depth_sort(a, b):
+	return b.get_index() < a.get_index()
